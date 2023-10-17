362d4f88b46d53a5d2bfa8e1446bfbed2717b0aa
johnny-shaman <johnny.shaman.saxophonist@gmail.com>
2023-10-17 11:15:49 +0900

    modified:   src/javascript/tokenizer.js 	new file:   src/regex.txt

--- src/javascript/tokenizer.js
+++ src/javascript/tokenizer.js
@@ -1,11 +1,11 @@
-function Tokenizer (string) {
-    return string
-    .replaceAll(/(\[`.+`\])|_[\W]+_|[\w]+/g, " $& ")
-    .replaceAll("\n"," ")
-    .split(/ +/g)
-    .filter(x => x.length);
-}
-
-
+const sign = (code) => ({
+    split () {
+        return code
+        .replaceAll(/(\[`.+`\])|_[\W]+_|[\w]+/g, " $& ")
+        .replaceAll("\n"," ")
+        .split(/ +/g)
+        .filter(x => x.length);
+    }
+});
 
 
