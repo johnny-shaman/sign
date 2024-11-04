61529529ef76abe02162ce563dc3b2e2239d5b4a
johnny-shaman <johnny.shaman.saxophonist@gmail.com>
2024-11-04 13:10:17 +0900

    implement progress about tokenizer.js

--- javascript/src/tokenizer.js
+++ javascript/src/tokenizer.js
@@ -1,18 +1,38 @@
 const {remove, lift, compareChain, regex} = requier('./tool.js')
 
-module.exports = async function processTokens(reader) {
+module.exports = async function processTokens(reader, root) {
   const tokens = compareChain(
     lift(
+      regex.letter,
       remove(
         regex.comment,
         await reader.readLine()
       )
     )
+    .map(
+      o => typeof o === 'string' ? lift(regex.string) : o
+    )
+    .map(
+      o => typeof o === 'string' ? lift(regex.number) : o
+    )
+    .map(
+      o => typeof o === 'string' ? lift(regex.hex) : o
+    )
+    .map(
+      o => typeof o === 'string' ? lift(regex.oct) : o
+    )
+    .map(
+      o => typeof o === 'string' ? lift(regex.bit) : o
+    )
+    .map(
+      o => typeof o === 'string' ? lift(regex.identifier) : o
+    )
+    .map(
+      o => typeof o === 'string' ? lift(regex.unit) : o
+    )
   );
 
-  // トークン列の性質に応じて適切な構造を返す
-  // childrenを持つべきかどうかも、この関数の中で判断する
   return tokens[tokens.length - 1] === ':' || tokens[tokens.length - 1] === '?' 
-    ? { tokens, children: processTokens(reader) }
+    ? { tokens, children: processTokens(reader, root) }
     : { tokens };
 }
