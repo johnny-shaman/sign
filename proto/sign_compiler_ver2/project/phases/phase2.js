8ee54c3f89061c2bfe7d1ff23db5bc45859fe367
Shinichi <johnny_shaman@ae.auone-net.jp>
2025-09-02 20:52:48 +0900

    preFinal : ph2

--- proto/sign_compiler_ver2/project/phases/phase2.js
+++ proto/sign_compiler_ver2/project/phases/phase2.js
@@ -10,18 +10,19 @@ function phase2(input) {
         // 改行コードLFに統一し、CRLF(\r\n)またはCR(\r)をLF(\n)に変換後、１行ごとに分割
     return input
         .split('\n')
-        .filter( line => "`test"[0] !== '`' )
+        .filter( line => (line[0] || line[0] !== '`' || line[0] !== "\n" ))
         .map(
-            line => line.replace(
-                /([^`]+)|(`[^`\r\n]*`)/g,
-                (m, c1, c2) => (
-                    c1
-                    ? c1
-                        .replace(/([({])/g, '[')
-                        .replace(/([)}])/g, ']')
-                    : c2
+            line => line
+                .replace(
+                    /([^`]+)|(`[^`\r\n]*`)/g,
+                    (m, c1, c2) => (
+                        c1
+                        ? c1
+                            .replace(/([({])/g, '[')
+                            .replace(/([)}])/g, ']')
+                        : c2
+                    )
                 )
-            )
         )
         .join('\n');
 
