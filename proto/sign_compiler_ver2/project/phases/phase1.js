099adbe8eabde368dcd5da8e07e8b49b71e8d107
Shinichi <johnny_shaman@ae.auone-net.jp>
2025-09-02 16:51:15 +0900

    update : phase1 & 2

--- proto/sign_compiler_ver2/project/phases/phase1.js
+++ proto/sign_compiler_ver2/project/phases/phase1.js
@@ -3,13 +3,13 @@
 
 /**
  * Phase1の文字列と文字トークンにカッコ付け処理を実行
- * @param {string} input - Phase1で処理されたコード
+ * @param {string} input - Phase1で処理されるコード
  * @returns {string} - トークンにカッコを付けたコード
  */
 function phase1(input) {
     return input
     .replace(/((?!^[`].*)(`[^`\r\n]*`))|\\./gm, '[$&]')
-    .replace(/(\r\n)|[\r\n]/g, '[$&]');
+    .replace(/\\(\r\n)|[\r\n]/g, '[$&]');
 }
 
 module.exports = { phase1 };
